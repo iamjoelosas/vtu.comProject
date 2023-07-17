97e6789428b115cb62d5725300ae8c0fca549499
iamjoelosas <joeldavidenabulele@gmail.com>
2023-07-17 11:01:57 -0400

    JoelCommit

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="./style.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <nav class="navTag">
+            <div class="mobileTag">
+                <i class="fa fa-bars"></i>
+            </div>
+            <div class="navList"> 
+                <ul class="ulList">
+                    <li><i class="fa fa-angle-right"></i> Dashboard</li>
+                    <li><i class="fa fa-angle-right"></i> System User</li>
+                    <li><i class="fa fa-angle-right"></i> Credit User</li>
+                    <li><i class="fa fa-angle-right"></i> Transaction</li>
+                    <li><i class="fa fa-angle-right"></i> Services</li>
+                    <li><i class="fa fa-angle-right"></i> SMS Portal</li>
+                    <li><i class="fa fa-angle-right"></i> Site Settings</li>
+                    <li><i class="fa fa-angle-right"></i> Send Mail</li>
+                    <li><i class="fa fa-angle-right"></i> Messages</li>
+                    <li><i class="fa fa-angle-right"></i> Account Details</li>
+                    <li><i class="fa fa-angle-right"></i> User List</li>
+                    <li><i class="fa fa-angle-right"></i> Usage Account</li>
+                    <li><i class="fa fa-angle-right"></i> Report</li>
+                    <li><i class="fa fa-angle-right"></i> Logout</li>
+                </ul>
+            </div>
+        </nav>
+        <main class="mainCon">
+            <header>
+                <div class="headerClass">
+                    <i class="fa fa-envelope"></i>
+                    <i class="fa fa-user-o"></i>
+                </div>
+                <div class="mainTitle">
+                    <p>Hi Admin</p>
+                    <p><i class="fa fa-home"></i> | Adeniyi</p>
+                </div>
+            </header>
+            
+            <section class="mainConSection">
+                <section class="innerSection">
+                    <div class="itemDiv">
+                        <div class="itemDivIn">
+                            <i class="fa fa-chain (allas)"></i>
+                            <div>
+                                <h3 class="itemH3">Users Cash</h3>
+                                <p>Tetre</p>
+                                <p class="itemBoldp">3456723</p>
+                            </div>
+                        </div>
+                        <div class="itemDivIn">
+                            <i class="fa fa-chain (allas)"></i>
+                            <div>
+                                <h3 class="itemH3">Users Cash</h3>
+                                <p>Tetre</p>
+                                <p class="itemBoldp">3456723</p>
+                            </div>
+                        </div>
+                        <div class="itemDivIn">
+                            <i class="fa fa-chain (allas)"></i>
+                            <div>
+                                <h3 class="itemH3">Users Cash</h3>
+                                <p>Tetre</p>
+                                <p class="itemBoldp">3456723</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="itemDiv">
+                        <div class="itemDivIn money">
+                            <i class="fa fa-money"></i>
+                            <div>
+                                <h3 class="itemH3">Users Cash</h3>
+                                <p>Tetre</p>
+                                <p class="itemBoldp">3456723</p>
+                            </div>
+                        </div>
+                        <div class="itemDivIn money">
+                            <i class="fa fa-money"></i>
+                            <div>
+                                <h3 class="itemH3">Users Cash</h3>
+                                <p>Tetre</p>
+                                <p class="itemBoldp">3456723</p>
+                            </div>
+                        </div>
+                        <div class="itemDivIn money">
+                            <i class="fa fa-money"></i>
+                            <div>
+                                <h3 class="itemH3">Users Cash</h3>
+                                <p>Tetre</p>
+                                <p class="itemBoldp">3456723</p>
+                            </div>
+                        </div>
+                        <div class="itemDivIn money">
+                            <i class="fa fa-money"></i>
+                            <div>
+                                <h3 class="itemH3">Users Cash</h3>
+                                <p>Tetre</p>
+                                <p class="itemBoldp">3456723</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="userDetail">
+                        <div class="userDetailIn">
+                            <div class="userDetailInText">
+                                <p>Adeoluwa</p>
+                                <button class="userDetBtn">Info</button>
+                            </div>
+                            <i class="fa fa-users"></i>
+                        </div><div class="userDetailIn">
+                            <div class="userDetailInText">
+                                <p>Adeoluwa</p>
+                                <button class="userDetBtn">Info</button>
+                            </div>
+                            <i class="fa fa-users"></i>
+                        </div>
+                        <div class="userDetailIn">
+                            <div class="userDetailInText">
+                                <p>Adeoluwa</p>
+                                <button class="userDetBtn">Info</button>
+                            </div>
+                            <i class="fa fa-users"></i>
+                        </div>
+                        <div class="userDetailIn">
+                            <div class="userDetailInText">
+                                <p>Adeoluwa</p>
+                                <button class="userDetBtn">Info</button>
+                            </div>
+                            <i class="fa fa-users"></i>
+                        </div>
+                    </div>
+
+                    <div class="userDetail">
+                        <div class="userDetailIn">
+                            <div class="userDetailInText">
+                                <p>Adeoluwa</p>
+                                <button class="userDetBtn sndndusrdet">Info</button>
+                            </div>
+                            <i class="fa fa-users"></i>
+                        </div><div class="userDetailIn">
+                            <div class="userDetailInText">
+                                <p>Adeoluwa</p>
+                                <button class="userDetBtn sndndusrdet">Info</button>
+                            </div>
+                            <i class="fa fa-users"></i>
+                        </div>
+                        <div class="userDetailIn">
+                            <div class="userDetailInText">
+                                <p>Adeoluwa</p>
+                                <button class="userDetBtn sndndusrdet">Info</button>
+                            </div>
+                            <i class="fa fa-users"></i>
+                        </div>
+                    </div>
+                </section>
+            </section>         
+            <section>
+                <div class="sectionTableDiv">
+                    
+                    <div class="tableP"><p>Id Transaction Details</p></div>
+                    <div class="tableDiv">
+                    <p>Show: </p>
+                    <select>
+                        <option>Lagos</option>
+                        <option>Lagos</option>
+                        <option>Lagos</option>
+                        <option>Lagos</option>
+                    </select>
+                    <p>Searches</p>
+                    <input type="search"/>
+                        <table class="vtu_table">
+                            <tr class="vtu_tr">
+                                <th class="vtu_td">Id</th>
+                                <th class="vtu_td">Id</th>
+                                <th class="vtu_td">Id</th>
+                                <th class="vtu_td">Id</th>
+                                <th class="vtu_td">Id</th>
+                                <th class="vtu_td">Id</th>
+                                <th class="vtu_td" style="text-align:center;">Action</th>
+                            </tr>
+                            <tr class="vtu_tr">
+                                <td class="vtu_td">1</td>
+                                <td class="vtu_td">1</td>
+                                <td class="vtu_td">1</td>
+                                <td class="vtu_td">1</td>
+                                <td class="vtu_td">1</td>
+                                <td class="vtu_td">1</td>
+                                <td style="text-align:center; padding:10px;"><i class="fa fa-remove"></i> <i class="fa fa-check"></i></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </section>          
+        </main>
+        <script src="script.js"></script>
+    </body>
+</html>
